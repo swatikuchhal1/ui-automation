e6975ff21838e9256043cc3f072086936d665606
swatikuchhal1 <97093295+swatikuchhal1@users.noreply.github.com>
2022-01-04 15:26:51 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+export const env = "https://app.sit1.vcilabs.com/"
+export const username = "swati.kuchhal-1@dev.local"
+export const password = "Password1"
+export const timeout = 500000
+export const imageMatch = 'false'
+export const panel = 'swati'
+export const takeScreenshots = 'true'
+export const surveyName = 'Survey Name-option-2'
+
+//Default Master screenshots resolution: await page.setViewport({ width: 1366, height: 768, deviceScaleFactor: 0.50 })
+
